da6bd682ba88ab15190b5402d26785dad4239371
SrijitaCTS <Srijita.Baksi@cognizant.com>
2026-05-12 10:06:21 +0530

    Create hello.bat

---
+++ hello.bat
@@ -0,0 +1,15 @@
+@echo off 
+
+echo ==================================== 
+
+echo   Hello from GitHub via Jenkins! 
+
+echo ==================================== 
+
+echo Build was triggered automatically. 
+
+echo Repository: jenkins-demo 
+
+echo Build Number: %BUILD_NUMBER% 
+
+echo ====================================
